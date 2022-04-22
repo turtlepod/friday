b46f377a0ec642c96cf7016364455f81d60f7968
turtlepod <david@shellcreeper.com>
2022-04-22 22:43:21 +0700

    Remove tamatebako_minimum_requirement(). Use WP theme style.css header to set theme requirements.

--- functions.php
+++ functions.php
@@ -1,27 +1,17 @@
 <?php
 /**
  * Theme Functions
-** ---------------------------- */
+ *
+ * @package FridayTheme
+ */
 
-/* Load Library. */
-require_once( trailingslashit( get_template_directory() ) . 'library/tamatebako.php' );
-
-/* Load theme general setup */
-add_action( 'after_setup_theme', 'friday_theme_setup', 5 );
+// Load library.
+require_once get_template_directory() . '/library/tamatebako.php';
 
 /**
- * Setup
+ * Theme Setup
  */
 function friday_theme_setup(){
-
-	/* === MINIMUM SYSTEM REQ === */
-	$back_compat_args = array(
-		'theme_name'   => 'Friday',
-		'wp_requires'  => '4.1.0',
-		'php_requires' => '5.3.0',
-	);
-	add_theme_support( 'tamatebako-back-compat', $back_compat_args );
-	if( ! tamatebako_minimum_requirement( $back_compat_args ) ) return;
 
 	/* === TRANSLATION === */
 	tamatebako_include( 'includes/translation' );
@@ -43,7 +33,5 @@ function friday_theme_setup(){
 
 	/* === BACKGROUND === */
 	tamatebako_include( 'includes/background' );
-
 }
-
-do_action( 'tamatebako_after_setup' );
+add_action( 'after_setup_theme', 'friday_theme_setup', 5 );
